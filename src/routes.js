a4dc2a76ab07d7b8398f30923b4175588d6330cd
kyon <kyon0304@gmail.com>
2015-07-29 17:58:56 +0800

    add komic viewer

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,6 @@ import ReaderView from './views/reader'
 export default (
    <Route path="/" handler={ AppView }>
      <DefaultRoute name="home" handler={ BookIntroView }/>
-     <Route name="reader" path="/reader" handler={ ReaderView } />
+     <Route name="reader" path="/reader/:page" handler={ ReaderView } />
    </Route>
 )
--- src/views/reader.js
+++ src/views/reader.js
@@ -3,11 +3,18 @@ import { Link } from 'react-router'
 
 export default class extends React.Component {
   render() {
+    var page = 0
+    if (this.props.params.page != undefined)
+      page = this.props.params.page
+
     return (
       <div>
         <Link to="home">返回</Link>
         <br/>
         <h1> #TODO 这是漫画阅读器 </h1>
+        <div>
+          <img src= { './images/' + page + '.jpeg' } />
+        </div>
       </div>
     )
   }
