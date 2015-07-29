a024e57a77f5481c0fa459caaa66aa8350313b51
hxgdzyuyi <hxgdzyuyi@gmail.com>
2015-07-29 13:57:27 +0800

    Fix react's warning: Unsupported style property
    Warning: Unsupported style property vertical-align. Did you mean
    verticalAlign?

--- src/widgets/vertical_align_middle.js
+++ src/widgets/vertical_align_middle.js
@@ -3,7 +3,7 @@ import React from 'react'
 // TODO(yangqing): use `flex-box` instead of `table`
 
 var wrapperStyle = { display: 'table' }
-  , cellStyle = { display: 'table-cell', 'vertical-align': 'middle' }
+  , cellStyle = { display: 'table-cell', verticalAlign: 'middle' }
 
 export default class extends React.Component {
   render() {
