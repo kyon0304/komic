3c968206eb7cc9c985bb816383de403bf19c1aa7
hxgdzyuyi <hxgdzyuyi@gmail.com>
2015-08-14 12:37:52 +0800

    将 webpack.config 修改成函数生成配置

--- webpack.config.js
+++ webpack.config.js
@@ -4,49 +4,51 @@ var webpack = require('webpack')
   , bower_components = path.join(__dirname, 'bower_components')
   , HtmlWebpackPlugin = require('html-webpack-plugin')
 
-module.exports = {
-  context: js_root
-, entry: [
-    'webpack/hot/only-dev-server'
-  , path.join(js_root, 'main.js')
-  ]
-, output: {
-    filename: '[name].js'
-  , publicPath: '/'
-  }
-, resolve: {
-    root: [js_root, bower_components]
-  , modulesDirectories: ["node_modules", "bower_components"]
-  }
-, module: {
-    loaders: [
-      { test: /\.(js|jsx)$/
-      , exclude: /node_modules/
-      , loader: 'react-hot!babel-loader'
-      , include: js_root
-      }
-    , { test: /\.styl/
-      , loader: 'style-loader!css-loader!stylus-loader' }
-    , { test: /\.css$/
-      , loader: 'style-loader!css-loader' }
-    , { test: /\.(png|jpg|woff|woff2)$/
-      , loader: 'url-loader?limit=8192'
-      }
+module.exports = function() {
+  return {
+    context: js_root
+  , entry: [
+      'webpack/hot/only-dev-server'
+    , path.join(js_root, 'main.js')
+    ]
+  , output: {
+      filename: '[name].js'
+    , publicPath: '/'
+    }
+  , resolve: {
+      root: [js_root, bower_components]
+    , modulesDirectories: ["node_modules", "bower_components"]
+    }
+  , module: {
+      loaders: [
+        { test: /\.(js|jsx)$/
+        , exclude: /node_modules/
+        , loader: 'react-hot!babel-loader'
+        , include: js_root
+        }
+      , { test: /\.styl/
+        , loader: 'style-loader!css-loader!stylus-loader' }
+      , { test: /\.css$/
+        , loader: 'style-loader!css-loader' }
+      , { test: /\.(png|jpg|woff|woff2)$/
+        , loader: 'url-loader?limit=8192'
+        }
+      ]
+    }
+  , babel: {
+     optional: ["runtime", "es7.functionBind", "es7.decorators", "es7.objectRestSpread"]
+    }
+  , stylus: {
+      use: [ (require('nib'))() ]
+    }
+  , plugins: [
+      new webpack.HotModuleReplacementPlugin()
+    , new webpack.NoErrorsPlugin()
+    , new webpack.ResolverPlugin(
+        new webpack.ResolverPlugin
+          .DirectoryDescriptionFilePlugin("bower.json", ["main"])
+      )
+    , new HtmlWebpackPlugin()
     ]
   }
-, babel: {
-   optional: ["runtime", "es7.functionBind", "es7.decorators", "es7.objectRestSpread"]
-  }
-, stylus: {
-    use: [ (require('nib'))() ]
-  }
-, plugins: [
-    new webpack.HotModuleReplacementPlugin()
-  , new webpack.NoErrorsPlugin()
-  , new webpack.ResolverPlugin(
-      new webpack.ResolverPlugin
-        .DirectoryDescriptionFilePlugin("bower.json", ["main"])
-    )
-  , new HtmlWebpackPlugin()
-  ]
 }
